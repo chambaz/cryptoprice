a7597d77b572dea8808e2478166a2de6ddf8cadd
Adam Chambers <adammchambers@gmail.com>
2017-05-29 10:36:54 -0400

    Set up basic component fetching btc / eth price in USD and printing to the screen

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,24 @@
-export default () => (
-  <div>Welcome to next.js!</div>
-)
+import React from 'react'
+import 'isomorphic-fetch'
+
+export default class MyPage extends React.Component {
+  static async getInitialProps () {
+    const res = await fetch('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD')
+    const json = await res.json()
+    return {
+      btc: json.BTC.USD,
+      eth: json.ETH.USD
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <ul>
+          <li>Bitcoin {this.props.btc}</li>
+          <li>Ethereum {this.props.eth} </li>
+        </ul>
+      </div>
+    )
+  }
+}
