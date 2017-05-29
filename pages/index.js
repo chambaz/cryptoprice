e0eb6049fc5d6b8c4430f8244cd69ea0b647dca4
Adam Chambers <adammchambers@gmail.com>
2017-05-29 15:19:41 -0400

    Add $ back to price after formatting value

--- pages/index.js
+++ pages/index.js
@@ -70,7 +70,7 @@ export default class CryptoPrice extends React.Component {
               transition: '0.8s ease-out'
             }}
             duration={1000}
-            formatValue={n => formatCurrency(n)}
+            formatValue={n => `$${formatCurrency(n)}`}
           />
         </h1>
         <h1
@@ -81,7 +81,7 @@ export default class CryptoPrice extends React.Component {
               transition: '0.8s ease-out'
             }}
             duration={1000}
-            formatValue={n => formatCurrency(n)}
+            formatValue={n => `$${formatCurrency(n)}`}
           />
         </h1>
       </div>
