bb995029a5382a8003cd9475a2f986ad2d7aeef4
Adam Chambers <adammchambers@gmail.com>
2017-05-29 10:59:22 -0400

    Set up toggle for btc / eth prices

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,39 @@
 import React from 'react'
 import 'isomorphic-fetch'
 
-export default class MyPage extends React.Component {
+export default class CryptoPrice extends React.Component {
   static async getInitialProps () {
     const res = await fetch('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD')
     const json = await res.json()
     return {
       btc: json.BTC.USD,
-      eth: json.ETH.USD
+      eth: json.ETH.USD,
+      show: 'eth'
     }
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      btc: this.props.btc,
+      eth: this.props.eth,
+      show: 'eth'
+    }
+  }
+
+  show(type) {
+    this.setState({show: type})
   }
 
   render () {
     return (
       <div>
         <ul>
-          <li>Bitcoin {this.props.btc}</li>
-          <li>Ethereum {this.props.eth} </li>
+          <li><a onClick={e => { e.preventDefault(); this.show('btc') }} href="#">Bitcoin</a></li>
+          <li><a onClick={e => { e.preventDefault(); this.show('eth') }} href="#">Ethereum</a> </li>
         </ul>
+        <h1 style={{display: (this.state.show === 'btc' ? 'block' : 'none')}}>{this.state.btc}</h1>
+        <h1 style={{display: (this.state.show === 'eth' ? 'block' : 'none')}}>{this.state.eth}</h1>
       </div>
     )
   }
